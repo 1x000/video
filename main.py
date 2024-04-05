cda682d53a3672d961816a1d8c2a0d4823c10fd0
1x000 <huaweilaomke402@gmail.com>
2024-04-05 22:03:24 +0700

    3

--- main.py
+++ main.py
@@ -9,12 +9,10 @@ from htmlmin import minify
 DIRECTORY = './'
 FILE_TYPES = ['video', 'image', 'document']
 OUTPUT_FILE = 'index.html'
-WALINE_SERVER_URL = 'https://video-pl.vercel.app'
 
 # Jinja2模板加载器
 env = Environment(loader=FileSystemLoader('templates'))
 template = env.get_template('index.html')
-
 
 # 提取视频标题的函数
 def extract_video_title(video_path):
@@ -26,7 +24,6 @@ def extract_video_title(video_path):
     except Exception:
         return os.path.splitext(os.path.basename(video_path))[0]
 
-
 # 生成文件信息列表
 files = []
 for root, dirs, files_in_dir in os.walk(DIRECTORY):
@@ -35,7 +32,7 @@ for root, dirs, files_in_dir in os.walk(DIRECTORY):
         file_mime = mimetypes.guess_type(file_path)[0]
         if file_mime:
             for file_type in FILE_TYPES:
-                if file_mime.startswith(file_type):
+                if file_mime.startswith(file_type) or (file_type == 'video' and file_path.endswith('.m3u8')):
                     file_size = os.path.getsize(file_path)
                     file_mtime = os.path.getmtime(file_path)
                     file_mtime_str = datetime.fromtimestamp(file_mtime).strftime('%Y-%m-%d %H:%M:%S')
@@ -50,7 +47,7 @@ for root, dirs, files_in_dir in os.walk(DIRECTORY):
                     files.append(file_info)
 
 # 渲染模板并混淆HTML和JavaScript
-html = template.render(files=files, waline_server_url=WALINE_SERVER_URL, mimetypes=mimetypes)
+html = template.render(files=files, mimetypes=mimetypes)
 minified_html = minify(html, remove_comments=True, remove_empty_space=True, remove_all_empty_space=True)
 
 # 写入HTML文件
